73c389e7b45f8f4706e36a1cb06df898cf7ae498
yukihide.suda <yuk_da@yahoo.co.jp>
2019-07-15 10:12:55 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 須田幸秀 on 2019/07/15.
+//  Copyright © 2019 yukihide.suda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
